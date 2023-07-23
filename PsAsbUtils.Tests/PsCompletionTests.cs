b0e314cdf103fc91795e70c18673cd0577c34627
Vasily Larionov <[none]>
2023-07-23 20:58:04 +0300

    Update test

--- PsAsbUtils.Tests/PsCompletionTests.cs
+++ PsAsbUtils.Tests/PsCompletionTests.cs
@@ -9,33 +9,32 @@ namespace PsAsbUtils.Tests;
 
 public class PsCompletionTests
 {
-    public class ServiceBusAdminMock : ServiceBusAdministrationClient
-    {
-        public override AsyncPageable<QueueProperties> GetQueuesAsync(CancellationToken cancellationToken = default)
-        {
-            var responseList = new List<QueueProperties>();
-            for (int i = 0; i < 10; i++)
-            {
-                responseList.Add(ServiceBusModelFactory.QueueProperties(
-                    name:$"UnitTest{i}",
-                    lockDuration:TimeSpan.FromMinutes(5),
-                    defaultMessageTimeToLive:TimeSpan.FromMinutes(10),
-                    autoDeleteOnIdle:TimeSpan.FromMinutes(10),
-                    duplicateDetectionHistoryTimeWindow:TimeSpan.FromMinutes(30),
-                    maxDeliveryCount:2,
-                    userMetadata:"Test"));
-            }
-
-            var page = Page<QueueProperties>.FromValues(responseList, null, new Mock<Response>().Object);
-            return AsyncPageable<QueueProperties>.FromPages(new[] { page });
-        }
-    }
-
     [Fact]
     public void CompletionReturned()
     {
-        var serviceBusAdmin = new ServiceBusAdminMock();
-        var completionEngine = new PsCompletion(serviceBusAdmin);
+        var serviceBusAdminMock = new Mock<ServiceBusAdministrationClient>();
+        serviceBusAdminMock
+                .Setup(x => x.GetQueuesAsync(It.IsAny<CancellationToken>()))
+                .Returns(() =>
+                {
+                    var responseList = new List<QueueProperties>();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        responseList.Add(ServiceBusModelFactory.QueueProperties(
+                            name: $"UnitTest{i}",
+                            lockDuration: TimeSpan.FromMinutes(5),
+                            defaultMessageTimeToLive: TimeSpan.FromMinutes(10),
+                            autoDeleteOnIdle: TimeSpan.FromMinutes(10),
+                            duplicateDetectionHistoryTimeWindow: TimeSpan.FromMinutes(30),
+                            maxDeliveryCount: 2,
+                            userMetadata: "Test"));
+                    }
+
+                    var page = Page<QueueProperties>.FromValues(responseList, null, new Mock<Response>().Object);
+                    return AsyncPageable<QueueProperties>.FromPages(new[] { page });
+                });
+
+        var completionEngine = new PsCompletion(serviceBusAdminMock.Object);
         var connectionMock = new Mock<IServiceBusConnection>();
         var completions = completionEngine.QueueCompletion(connectionMock.Object, "Unit.*", 2);
         Assert.Equal(2, completions.Count());
